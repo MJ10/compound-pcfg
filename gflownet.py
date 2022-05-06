expose temp to cmd line; fix NT off-by-one bug

gflownet.py
@@ -122,14 +122,15 @@ class segmenter_controller():
                     action : str,
                     F_logits: torch.Tensor,
                     states: torch.Tensor,
-                    temperature_pos : float = 1.,):
+                    temperature_pos : float = 1.,
+                    temperature_tok : float = 1.,):
         if type(states) is not list:
             # convert states to a list and remove padding
             states = [sent[sent!=self.pad_sym] for sent in states]
             convert_to_tensor = True
         else:
             convert_to_tensor = False
-        F_actions = self._sample_forward_actions(action, F_logits, states, temperature_pos)
+        F_actions = self._sample_forward_actions(action, F_logits, states, temperature_pos, temperature_tok)
         P_F = self.calc_forward_prob(F_logits=F_logits,
                                     F_actions=F_actions,
                                     states=states)
@@ -253,7 +254,7 @@ class segmenter_controller():
                 states[i] = torch.cat([states[i][:pos+1], torch.zeros(1).to(self.device)+self.split_sym, states[i][pos+1:]], dim=0).long()
             elif action == "tag":
                 # change the pos-th split symbol to tok
-                states[i][pos] = tok+self.n_vocab # need to verify that this actually modifies states
+                states[i][pos] = tok+self.n_vocab+1 # need to verify that this actually modifies states
         return states
 
     @torch.no_grad()
@@ -417,7 +418,7 @@ class segmenter_controller():
             p = [-1] + nt_positions.cpu().numpy().flatten().tolist()
             spans += [ seq[p[i]+1:p[i+1]] for i in range(len(p) - 1) ]
             # import pdb; pdb.set_trace();
-            tag_seqs.append(seq[nt_positions].flatten() - self.n_vocab)
+            tag_seqs.append(seq[nt_positions].flatten() - self.n_vocab - 1)
 
         x = torch.nn.utils.rnn.pad_sequence(spans, batch_first=True)
         lengths = torch.Tensor(list(map(len, spans))).to(x.device).long()

train_gfn.py
@@ -50,6 +50,9 @@ parser.add_argument('--batch_size', default=4, type=int, help='which gpu to use'
 parser.add_argument('--seed', default=3435, type=int, help='random seed')
 parser.add_argument('--print_every', type=int, default=1000, help='print stats after N batches')
 parser.add_argument('--minimal_dataloader', action="store_true")
+# GFN options
+parser.add_argument('--temperature_pos', default=1., type=float)
+parser.add_argument('--temperature_tok', default=1., type=float)
 
 def main(args):
   np.random.seed(args.seed)
@@ -118,11 +121,11 @@ def main(args):
   gfn_forward_tag.cuda()
   gfn_backward.train()
   gfn_backward.cuda()
-  gfn_optimizer = torch.optim.Adam([{'params':gfn_Z.parameters(), 'lr':args.lr, 'betas':(args.beta1, args.beta2)},
-                                    {'params':gfn_encoder.parameters(), 'lr':args.lr, 'betas':(args.beta1, args.beta2)},
-                                    {'params':gfn_forward_split.parameters(), 'lr':args.lr, 'betas':(args.beta1, args.beta2)},
-                                    {'params':gfn_forward_tag.parameters(), 'lr':args.lr, 'betas':(args.beta1, args.beta2)},
-                                    {'params':gfn_backward.parameters(), 'lr':args.lr, 'betas':(args.beta1, args.beta2)}])
+  gfn_optimizer = torch.optim.Adam([{'params':gfn_Z.parameters(), 'lr':0.1*args.lr, 'betas':(args.beta1, args.beta2)},
+                                    {'params':gfn_encoder.parameters(), 'lr':0.1*args.lr, 'betas':(args.beta1, args.beta2)},
+                                    {'params':gfn_forward_split.parameters(), 'lr':0.1*args.lr, 'betas':(args.beta1, args.beta2)},
+                                    {'params':gfn_forward_tag.parameters(), 'lr':0.1*args.lr, 'betas':(args.beta1, args.beta2)},
+                                    {'params':gfn_backward.parameters(), 'lr':0.1*args.lr, 'betas':(args.beta1, args.beta2)}])
 
   best_val_ppl = 1e5
   best_val_f1 = 0
@@ -342,7 +345,8 @@ def sample_gfn(state, controller, gfn_Z, gfn_encoder, gfn_forward_split, gfn_for
     state, _, B_actions, _logPF = \
                 controller.sample_forward('split',
                                           gfn_forward_split(encoded_sents),
-                                          state)
+                                          state,
+                                          temperature_pos=args.temperature_pos,)
     pad_mask = torch.zeros_like(state).to(torch.float)
     pad_mask[state==pad_sym] = -float('inf')
     encoded_sents = gfn_encoder(state, pad_mask)
@@ -360,7 +364,9 @@ def sample_gfn(state, controller, gfn_Z, gfn_encoder, gfn_forward_split, gfn_for
     state, _, B_actions, _logPF = \
                 controller.sample_forward('tag',
                                           gfn_forward_tag(encoded_sents),
-                                          state)
+                                          state,
+                                          temperature_pos=args.temperature_pos,
+                                          temperature_tok=args.temperature_tok)
     encoded_sents = gfn_encoder(state, pad_mask)
     _logPB = controller.calc_backward_prob(gfn_backward(encoded_sents),
                                           state,